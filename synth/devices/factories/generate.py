c583abe1253a06031c74ed657a6fda5dbf75b311
Tom <tom.wallace@devicepilot.com>
2017-05-12 15:40:30 +0100

    Notion of factories.

---
+++ synth/devices/factories/generate.py
@@ -0,0 +1,9 @@
+from synth.devices.device import Device
+
+class Generate(Device):
+
+    def __init__(self, conf, engine, client):
+        super(Generate, self).__init__(conf, engine, client)
+
+        for
+
