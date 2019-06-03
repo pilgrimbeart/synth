201d47daa7bfa56173a51b9624dc0329869eda21
Pilgrim Beart <pilgrim@beart.org.uk>
2019-06-03 09:34:13 +0100

    Added random_lower/upper and randstruct

--- synth/devices/variable.py
+++ synth/devices/variable.py
@@ -8,8 +8,10 @@ Configurable parameters::
 
     {
         "name" : the name of the variable
-        "value" : a static number or string - or an array to pick one randomly
-        "timefunction" : a timefunction definition
+        "value" : a static number or string - or an array to pick one randomly OR
+        "timefunction" : a timefunction definition OR
+        "random_lower", "random_upper" : a range of values to pick randomly within (with optional "precision" parameter) OR
+        "randstruct" : "["A definition like this where",[" lists "," are "," concatenated"], " and tuples are ("chosen from","selected betwixt"), "randomly]"
     }
 
     -or-
@@ -28,6 +30,7 @@ import logging
 import random
 from device import Device
 from common import importer
+from helpers import randstruct
 
 class Variable(Device):
     def __init__(self, instance_name, time, engine, update_callback, context, params):
@@ -44,8 +47,18 @@ class Variable(Device):
                 var_value = importer.get_class("timefunction", tf_name)(engine, self, params["timefunction"][tf_name])
                 self.set_property(var_name, var_value.state())
                 engine.register_event_at(var_value.next_change(), self.tick_variable, (var_name, var_value), self)
+            elif "random_lower" in params:
+                lower = float(params["random_lower"])
+                upper = float(params["random_upper"])
+                precision = params.get("precision", 1)
+                var_value = lower + random.random() * (upper-lower)
+                var_value = int(var_value * precision) / float(precision)
+                self.set_property(var_name, var_value)
+            elif "randstruct" in params:
+                var_value = randstruct.evaluate(params["randstruct"])
+                self.set_property(var_name, var_value)
             else:
-                assert False,"variable " + var_name + " must have either value or timefunction"
+                assert False,"variable " + var_name + " must have either value, timefunction, random_lower/upper or randstruct"
             self.variables.append( (var_name, var_value) )
 
         super(Variable, self).__init__(instance_name, time, engine, update_callback, context, params)
