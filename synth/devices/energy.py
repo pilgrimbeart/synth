02482ac9605e31581a5363a90bd016e3aaa4c22e
Pilgrim Beart <pilgrim@beart.org.uk>
2019-08-30 12:37:11 +0100

    Fixed how kWh accumulates from kW (was wrongly assuming an interval of 1h)

--- synth/devices/energy.py
+++ synth/devices/energy.py
@@ -26,7 +26,7 @@ import time
 from device import Device
 import helpers.opening_times as opening_times
 
-ENERGY_READING_INTERVAL = 60 * 30
+ENERGY_READING_INTERVAL_S = 60 * 30
 DEFAULT_OPENING_TIMES = "nine_to_five"
 DEFAULT_MAX_POWER_KW = 10.0
 DEFAULT_BASELOAD_POWER_KW = 2.0
@@ -43,7 +43,7 @@ class Energy(Device):
             self.set_property("device_type", "energy")
         self.set_property("kWh", int(random.random() * 100000))
         self.set_property("occupied", False)    # !!!!!!!!!!! TEMP BODGE TO OVERCOME CLUSTERING PROBLEM
-        self.engine.register_event_in(ENERGY_READING_INTERVAL, self.tick_reading, self, self)
+        self.engine.register_event_in(ENERGY_READING_INTERVAL_S, self.tick_reading, self, self)
 
     def comms_ok(self):
         return super(Energy,self).comms_ok()
@@ -61,12 +61,12 @@ class Energy(Device):
         kW = self.baseload_power_kW + open_chance * (self.max_power_kW - self.baseload_power_kW - self.power_variation_kW/2.0)
         kW += random.random() * self.power_variation_kW
         kWh = self.get_property("kWh")
-        kWh += kW
+        kWh += kW * ENERGY_READING_INTERVAL_S / (60 * 60.0)
 
         kW = int(100 * kW) / 100.0   # Round
         kWh = int(100 * kWh) / 100.0
         self.set_property("kW", kW)
         self.set_property("kWh", kWh)
         self.set_property("occupied", not self.get_property("occupied"))    # !!!!!!!!!!! TEMP BODGE TO OVERCOME CLUSTERING PROBLEM
-        self.engine.register_event_in(ENERGY_READING_INTERVAL, self.tick_reading, self, self)
+        self.engine.register_event_in(ENERGY_READING_INTERVAL_S, self.tick_reading, self, self)
 
