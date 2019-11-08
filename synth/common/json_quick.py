ec11a8d0a17555677193b84439ca396ebb73aac2
Pilgrim Beart <pilgrim.beart@devicepilot.com>
2019-11-08 17:54:42 +0000

    Fall back to default json.dumps() on error (Unicode!)

--- synth/common/json_quick.py
+++ synth/common/json_quick.py
@@ -1,17 +1,23 @@
 """JSONquick
 Faster JSON dumps"""
 
+import json
+import logging
+
 def dumps(props):
-    s = "{"
-    for k,v in sorted(props.iteritems()):
-        typ = type(v)
-        s += '"' + k + '": '
-        if typ in [str, unicode]:
-            s += '"' + str(v) + '"'
-        elif typ == bool:
-            s += ["false","true"][v]
-        else:
-            s += str(v)
-        s += ", "
-    s = s[:-2] + "}"
-    return s
+    try:
+        s = "{"
+        for k,v in sorted(props.iteritems()):
+            typ = type(v)
+            s += '"' + k + '": '
+            if typ in [str, unicode]: 
+                s += '"' + str(v) + '"' #   Gets a decodeerror on some Unicode strings for reasons I can't fathom
+            elif typ == bool:
+                s += ["false","true"][v]
+            else:
+                s += str(v)
+            s += ", "
+        s = s[:-2] + "}"
+        return s
+    except:
+        return json.dumps(props, sort_keys = True) # Fall back to the slow way!
