167e8951074e2c8efe1569262833b1c67c73fd37
EC2 Default User <ec2-user@ip-172-31-13-252.eu-west-1.compute.internal>
2021-08-18 14:18:09 +0000

    Can now set energy reading interval

--- synth/devices/energy.py
+++ synth/devices/energy.py
@@ -6,6 +6,7 @@ Simulates energy meter (electricity or gas or both, with associated Comms module
 Configurable parameters::
 
     {
+        "reading_interval" : (optional) "PT12H" - how often a reading is sent. Defaults to half-hourly.
         "opening_times" : (optional) "name of an opening-times pattern"
         "max_power" : (optional) maximum power level
         "baseload_power" : (optional) baseload power level (e.g. night-time)
@@ -22,19 +23,18 @@ Device properties created::
 import random
 import logging
 import time
+import isodate
 
 from .device import Device
 from .helpers import opening_times as opening_times
 
-ENERGY_READING_INTERVAL_S = 60 * 30
+DEFAULT_ENERGY_READING_INTERVAL = "PT30M"
 DEFAULT_OPENING_TIMES = "nine_to_five"
 DEFAULT_MAX_POWER_KW = 10.0
 DEFAULT_BASELOAD_POWER_KW = 2.0
 DEFAULT_POWER_VARIATION_KW = 1.0
 
 READING_FAULT_DAILY_CHANCE = 1.0 / 10000
-
-READING_FAULT_CHANCE = READING_FAULT_DAILY_CHANCE / ((60 * 60 * 24.0) / ENERGY_READING_INTERVAL_S)
 
 class Energy(Device):
     def __init__(self, instance_name, time, engine, update_callback, context, params):
@@ -55,7 +55,8 @@ class Energy(Device):
         else:
             self.set_property("meter_type", "gas")
             self.set_property("icon", "flame")
-        self.engine.register_event_in(ENERGY_READING_INTERVAL_S, self.tick_reading, self, self)
+        self.energy_reading_interval_s = isodate.parse_duration(params["energy"].get("reading_interval", DEFAULT_ENERGY_READING_INTERVAL)).total_seconds()
+        self.engine.register_event_in(self.energy_reading_interval_s, self.tick_reading, self, self)
 
     def comms_ok(self):
         return super(Energy,self).comms_ok()
@@ -73,12 +74,13 @@ class Energy(Device):
         kW = self.baseload_power_kW + open_chance * (self.max_power_kW - self.baseload_power_kW - self.power_variation_kW/2.0)
         kW += random.random() * self.power_variation_kW
         kWh = self.get_property("kWh")
-        kWh += kW * ENERGY_READING_INTERVAL_S / (60 * 60.0)
+        kWh += kW * self.energy_reading_interval_s / (60 * 60.0)
 
         kW = int(100 * kW) / 100.0   # Round
         kWh = int(100 * kWh) / 100.0
 
-        if random.random() < READING_FAULT_CHANCE:
+        reading_fault_chance = READING_FAULT_DAILY_CHANCE / ((60 * 60 * 24.0) / self.energy_reading_interval_s)
+        if random.random() < reading_fault_chance:
             if random.random() > 0.5:
                 delta = 1
             else:
@@ -93,5 +95,5 @@ class Energy(Device):
         if self.occupied_bodge:
             self.set_property("occupied", not self.get_property("occupied"))    # !!!!!!!!!!! TEMP BODGE TO OVERCOME CLUSTERING PROBLEM
         self.end_property_group() # <--
-        self.engine.register_event_in(ENERGY_READING_INTERVAL_S, self.tick_reading, self, self)
+        self.engine.register_event_in(self.energy_reading_interval_s, self.tick_reading, self, self)
 
