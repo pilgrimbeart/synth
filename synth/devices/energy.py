238b7775409646c1441f7a828df4754c5aa2e7f5
EC2 Default User <ec2-user@ip-172-31-13-252.eu-west-1.compute.internal>
2022-10-19 15:41:00 +0000

    Added test mode

--- synth/devices/energy.py
+++ synth/devices/energy.py
@@ -12,6 +12,7 @@ Configurable parameters::
         "baseload_power" : (optional) baseload power level (e.g. night-time)
         "power_variation" : (optional) how much "noise" on the reading
         "no_metadata" : (optional) don't send metadata
+        "test_mode" : (optional) if true then kW starts at zero and rises slowly
     }
 
 Device properties created::
@@ -37,18 +38,26 @@ DEFAULT_POWER_VARIATION_KW = 1.0
 
 READING_FAULT_DAILY_CHANCE = 1.0 / 10000
 
+TEST_MODE_INC_KW_PER_DAY = 1.0
+
 class Energy(Device):
     def __init__(self, instance_name, time, engine, update_callback, context, params):
         super(Energy,self).__init__(instance_name, time, engine, update_callback, context, params)
+        self.start_time = engine.get_now()
         self.opening_times = params["energy"].get("opening_times", DEFAULT_OPENING_TIMES)
         self.max_power_kW = params["energy"].get("max_power", DEFAULT_MAX_POWER_KW)
         self.baseload_power_kW = params["energy"].get("baseload_power", DEFAULT_BASELOAD_POWER_KW)
         self.power_variation_kW = params["energy"].get("power_variation", DEFAULT_POWER_VARIATION_KW)
         self.no_metadata = params["energy"].get("no_metadata", False)
+        self.test_mode = params["energy"].get("test_mode", False)
         if not self.property_exists("device_type"):
             if not self.no_metadata:
                 self.set_property("device_type", "energy")
-        self.set_property("kWh", int(random.random() * 100000))
+        if self.test_mode:
+            kWh = 0
+        else:
+            kWh = int(random.random() * 100000)
+        self.set_property("kWh", kWh)
         self.occupied_bodge = params["energy"].get("occupied_bodge", False)
         if self.occupied_bodge:
             self.set_property("occupied", False)    # !!!!!!!!!!! TEMP BODGE TO OVERCOME CLUSTERING PROBLEM
@@ -72,17 +81,21 @@ class Energy(Device):
 
     # Private methods
     def tick_reading(self, _):
-        open_chance = opening_times.chance_of_occupied(self.engine.get_now(), self.opening_times)
-        kW = self.baseload_power_kW + open_chance * (self.max_power_kW - self.baseload_power_kW - self.power_variation_kW/2.0)
-        kW += random.random() * self.power_variation_kW
+        if self.test_mode:
+            kW = (self.engine.get_now() - self.start_time) * (TEST_MODE_INC_KW_PER_DAY / (60*60*24.0))
+        else:
+            open_chance = opening_times.chance_of_occupied(self.engine.get_now(), self.opening_times)
+            kW = self.baseload_power_kW + open_chance * (self.max_power_kW - self.baseload_power_kW - self.power_variation_kW/2.0)
+            kW += random.random() * self.power_variation_kW
+
         kWh = self.get_property("kWh")
         kWh += kW * self.energy_reading_interval_s / (60 * 60.0)
 
-        kW = int(100 * kW) / 100.0   # Round
+        kW = int(1000 * kW) / 1000.0   # Round to 1 Watt
         kWh = int(100 * kWh) / 100.0
 
         reading_fault_chance = READING_FAULT_DAILY_CHANCE / ((60 * 60 * 24.0) / self.energy_reading_interval_s)
-        if random.random() < reading_fault_chance:
+        if (not self.test_mode) and (random.random() < reading_fault_chance):
             if random.random() > 0.5:
                 delta = 1
             else:
